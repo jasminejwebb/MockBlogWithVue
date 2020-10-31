307d70a8358a6fd3d3070bf4124c114725c27cc6
Jasmine <jasminejwebb@gmail.com>
2020-10-31 05:07:16 +0000

    Followed README tutorial, working display, search and browse.

---
+++ src/views/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <ProductList :products="products" />
+</div>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'Browse',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+      country: '',
+    }
+  },
+  computed: {
+    products() {
+      return this.$root.$data.products.filter(product => product.country === this.country);
+    }
+  },
+  methods: {
+    select(country) {
+      this.country = country;
+    }
+  }
+}
+</script>
